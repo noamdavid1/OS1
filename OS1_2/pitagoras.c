8bf13a33adaed80aaace91b569d4c4b3906e7ed7
noamdavid1 <93923600+noamdavid1@users.noreply.github.com>
2024-01-23 18:38:43 +0200

    Add files via upload

--- OS1_2/pitagoras.c
+++ OS1_2/pitagoras.c
@@ -4,27 +4,27 @@
 #include <math.h>
 
 int main() {
-    // קליטת אורכי הצלעות מהמשתמש
+    // Receiving side lengths from the user
     double side1, side2, side3;
     printf("Enter the lengths of the three sides of the triangle: ");
     scanf("%lf %lf %lf", &side1, &side2, &side3);
 
-    // בדיקה האם מדובר בשלשה פיתגורית
+    // Checking whether it is a Pythagorean triple
     if (pow(side1, 2) + pow(side2, 2) == pow(side3, 2) ||
         pow(side1, 2) + pow(side3, 2) == pow(side2, 2) ||
         pow(side2, 2) + pow(side3, 2) == pow(side1, 2)) {
-        // המשולש הוא שלשה פיתגורית
+        //The triangle is a Pythagorean triangle
         double angle1, angle2, angle3;
 
-        // חישוב זוויות בראדיאנים
+        //Calculation of angles in radians
         angle1 = asin(side1 / side3);
         angle2 = asin(side2 / side3);
         angle3 = PI - angle1 - angle2;
 
-        // הצגת הזוויות במשולש
+        //Showing the angles in a triangle
         printf("Angles in radians: %.6lf %.6lf %.6lf\n", angle1, angle2, angle3);
     } else {
-        // לא מדובר בשלשה פיתגורית
+        // This is not a Pythagorean triangle
         printf("Error: Not a Pythagorean triangle\n");
     }
 
